a14d4488669fefba10f05e23295036fe6563d7c5
VictorLpr <victorlepron@gmail.com>
2025-05-26 13:32:21 +0200

    update: import first in app.js

--- app.mjs
+++ app.mjs
@@ -1,11 +1,10 @@
-import express from "express";const app = express();
-const port = 5000;
+import express from "express";
 import cors from "cors";
+
+const app = express();
+const port = 5000;
 app.use(cors());
 app.use(express.json());
-
-
-
 
 
 
