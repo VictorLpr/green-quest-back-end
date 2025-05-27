cf243e7ac45e974bf5f9c8ea3fda919b6dd062c6
VictorLpr <victorlepron@gmail.com>
2025-05-27 15:45:28 +0200

    add : volunteers route get by username & post

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
-const port = 5000;
+const cors = require("cors");
 app.use(cors());
+require('dotenv').config()
+const volunteersRoutes = require('./routes/volunteers');
 app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.use('/volunteers', volunteersRoutes )
 
 
 
---
+++ routes/volunteers.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../db')
+
+const filterByUsername = async (req, res, next) => {
+    const username = req.params.username;
+    try {
+        const values = [username]
+        let baseQuery = 'SELECT * FROM volunteers WHERE username = $1'
+        const result = await db.query(baseQuery, values)
+        if (result.rows[0]) {
+            req.findUser = result.rows[0]
+            next()
+        } else {
+            res.sendStatus(404)
+        }
+    } catch (err) {
+        res.sendStatus(500)
+    }
+}
+
+
+
+router.get('/:username', filterByUsername, async (req, res) => {
+
+     res.send(req.findUser)
+
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const {firstName, lastName, username, email, password, city } = req.body;
+        const cityQuery = `SELECT id FROM cities WHERE title = $1`;
+        const cityId = await db.query(cityQuery, [city]);
+        if (cityId.rows[0]) {
+            cityId = cityId.rows[0];
+        } else {
+            const createCity = await db.query(
+                `INSERT INTO cities (title) VALUES ($1) RETURNING id`,
+                [city]
+            );
+            cityId = createCity.rows[0];
+        }
+        const result = await db.query(
+            `INSERT INTO volunteers (firstname, lastname, username, email, password, city_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
+            [firstName, lastName, username, email, password, cityId]
+        )
+        res.status(201).json(result.rows[0])
+
+    } catch (err) {
+        res.sendStatus(500);
+    }
+})
