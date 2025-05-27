4c2b87464ffa8a8c883b75f14b2d5c99340fb3f5
VictorLpr <victorlepron@gmail.com>
2025-05-27 16:13:01 +0200

    update : port

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.use(cors());
 require('dotenv').config()
+
 const volunteersRoutes = require('./routes/volunteers');
+app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
+const port =  5000;
 
 app.use('/volunteers', volunteersRoutes )
 
