be159e92b972ec808793186bd7ffa550cf783d19
faty <145115890+fatoumatad223@users.noreply.github.com>
2025-05-30 15:45:31 +0200

    add: donations routes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 
 const volunteersRoutes = require('./routes/volunteers');
 const associationsRoutes = require('./routes/associations');
+const donationsRoutes = require('./routes/donations');
 const collectionsRoutes = require('./routes/collections');
 
 app.use(cors());
@@ -15,6 +16,8 @@ const port = 5001;
 app.use('/volunteers', volunteersRoutes)
 
 app.use('/associations', associationsRoutes)
+
+app.use('/donations', donationsRoutes)
 
 app.use('/collections', collectionsRoutes)
 
---
+++ routes/donations.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../db')
+
+
+router.get("/", async (req, res) => {
+    const result = await db.query("SELECT * FROM donations")
+    res.status(200).send(result.rows)
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const {volunteerId, associationId, value} = req.body
+        
+        const result = await db.query(
+            `INSERT INTO donations (volunteer_id, association_id) VALUES ($1, $2) RETURNING *`,
+            [volunteerId, associationId]
+        )
+
+        const donationId = result.rows[0].id
+        const donationValueQuery = await db.query(
+            `INSERT INTO donation_values(donation_id, value) VALUES ($1, $2) RETURNING *`,
+            [donationId, value]
+        )
+        res.sendStatus(201)
+
+    } catch (error) {
+        console.error('Erreur serveur:', err);
+        res.sendStatus(500);
+    }
+})
+
+module.exports = router;
