6b1da1b8a31924a75240a92f9ddb679ea1aebbca
VictorLpr <victorlepron@gmail.com>
2025-05-30 10:56:19 +0200

    update : port to 5001

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const associationsRoutes = require('./routes/associations');
 app.use(cors());
 app.use(express.json());
 
-const port = 5000;
+const port = 5001;
 
 app.use('/volunteers', volunteersRoutes)
 
