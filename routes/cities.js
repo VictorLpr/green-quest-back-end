64f5b368cc4e0a342f5498552b2e3691e59cab75
VictorLpr <victorlepron@gmail.com>
2025-06-09 12:39:29 +0200

    update cities route to get only distinct cities in volunteers table

--- routes/cities.js
+++ routes/cities.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const db = require('../db')
 
 router.get("/", async (req, res) => {
-    const result = await db.query("SELECT title, id FROM cities ORDER BY title ASC")
+    const result = await db.query("SELECT DISTINCT c.title, c.id FROM cities c INNER JOIN  volunteers v  ON c.id = v.city_id ORDER BY title ASC")
     res.status(200).send(result.rows)
 })
 
