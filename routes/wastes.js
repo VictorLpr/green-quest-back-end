1ef0146cfb213a440d25595013490fe75d52dd7e
VictorLpr <victorlepron@gmail.com>
2025-06-10 10:49:44 +0200

    update waste route order by id

--- routes/wastes.js
+++ routes/wastes.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const db = require('../db')
 
 router.get('/', async (req, res) => {
-    const result = await db.query('SELECT * FROM wastes')
+    const result = await db.query('SELECT * FROM wastes ORDER BY id')
+    console.log(result.rows)
     res.status(200).send(result.rows)
 })
 
