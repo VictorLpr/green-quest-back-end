f7a23b528677c4bd7dd50f646f0db752ecc06180
VictorLpr <victorlepron@gmail.com>
2025-06-18 13:57:32 +0200

    fix : get collections route : full waste array even if sum = 0

--- routes/collections.js
+++ routes/collections.js
@@ -1,112 +1,143 @@
-const express = require('express')
-const router = express.Router()
-const db = require('../db')
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
 
-const getOrCreateCity = require('../utils/city')
+const getOrCreateCity = require("../utils/city");
 
-router.get('/:id/:date', async (req, res) => { 
-    const { id, date } = req.params
-    const result = await db.query(`SELECT SUM(q.quantity), w.title FROM quantities q LEFT JOIN wastes w ON q.waste_id = w.id LEFT JOIN collections c ON q.collection_id = c.id WHERE c.volunteer_id = $1 AND DATE_TRUNC('month', c.created_at) = $2 GROUP BY w.title;`, [id, date])
-    res.status(200).send(result.rows)
-})
+router.get("/:id/:date", async (req, res) => {
+  const { id, date } = req.params;
+  const result = await db.query(
+    `SELECT w.id, w.title,COALESCE(SUM(q.quantity), 0) AS sum
+     FROM wastes w
+     LEFT JOIN (
+       SELECT q.*
+        FROM quantities q
+        INNER JOIN collections c 
+            ON q.collection_id = c.id
+        WHERE c.volunteer_id = $1
+        AND DATE_TRUNC('month', c.created_at) = $2
+     ) q ON q.waste_id = w.id
+    GROUP BY w.id
+    ORDER BY w.id ASC;`,
+    [id, date]
+  );
+  console.log(result.rows);
 
-router.post('/', getOrCreateCity, async (req, res) => {
-    const client = await db.connect();
-    try {
-        const { volunteerId, quantitiesArray, cityId } = req.body;
-        // quantitiesArray [
-        //    {
-        //         "wasteId": "_",
-        //         "wastePoints": "_",
-        //         "quantity": "_"
-        //     },
-        //     ...
-        // ]
+  res.status(200).send(result.rows);
+});
 
-        // check les entrées
-        if (!volunteerId) {
-            return res.status(400).json({ error: 'Volunteer id is missing' });
-        };
-        if (!Array.isArray(quantitiesArray) || quantitiesArray.length === 0) {
-            return res.status(400).json({ error: 'Quantities are missing' });
-        };
-        if (!cityId) {
-            return res.status(400).json({ error: 'City id is missing' });
-        };
+router.post("/", getOrCreateCity, async (req, res) => {
+  const client = await db.connect();
+  try {
+    const { volunteerId, quantitiesArray, cityId } = req.body;
+    // quantitiesArray [
+    //    {
+    //         "wasteId": "_",
+    //         "wastePoints": "_",
+    //         "quantity": "_"
+    //     },
+    //     ...
+    // ]
 
-        // Lance les query
-        await client.query('BEGIN');
-
-        // add collection
-        const insertCollectionQuery = `INSERT INTO collections (volunteer_id, city_id) VALUES ($1, $2) RETURNING id, created_at`;
-        // get city title
-        const selectCityTitleQuery = `SELECT title FROM cities WHERE id = $1`;
-        // add quantities
-        const insertQuantityQuery = `INSERT INTO quantities (quantity, waste_id, collection_id) VALUES ($1, $2, $3)`;
-        // get waste point value and title
-        const selectWasteQuery = `SELECT points_value, title, label FROM wastes WHERE id = $1`;
-        // get points volunteers already have
-        const selectVolunteerPointsQuery = `SELECT points FROM volunteers WHERE id = $1`;
-        // update volunteer / calculate total points
-        const updateVolunteerPointsQuery = `UPDATE volunteers SET points = $1 WHERE id = $2`;
-
-        const createCollecte = await client.query(insertCollectionQuery, [volunteerId, cityId]);
-        const collectionId = createCollecte.rows[0].id;
-        const collectionDate = createCollecte.rows[0].created_at;
-
-        const getCity = await client.query(selectCityTitleQuery, [cityId]);
-        const cityName = getCity.rows[0].title;
-
-        const getSoldePoints = await client.query(selectVolunteerPointsQuery, [volunteerId]);
-        let totalPoints = getSoldePoints.rows[0].points;
-
-        const collectWastes = [];
-        let pointsEarned = 0;
-
-        for (const quantity of quantitiesArray) {
-            await client.query(insertQuantityQuery, [quantity.quantity, quantity.wasteId, collectionId]);
-
-            const wastePoints = await client.query(selectWasteQuery, [quantity.wasteId]);
-            pointsEarned += wastePoints.rows[0].points_value * quantity.quantity;
-
-            // faire le tableau : waste title, quantity
-            collectWastes.push({ title: wastePoints.rows[0].title, label: wastePoints.rows[0].label, quantity: quantity.quantity });
-        }
-
-        totalPoints += pointsEarned;
-
-        await client.query(updateVolunteerPointsQuery, [totalPoints, volunteerId]);
-        await client.query('COMMIT');
-
-        const returnDatas = [
-            {
-                date: collectionDate,
-                cityName,
-                collectWastes
-            }
-        ]
-
-        // return [
-        //     {
-        //         date: "",
-        //         cityName: "",
-        //         [
-        //             {
-        //                 title: "",
-        //                 quantity: ""
-        //             },
-        //             ...
-        //         ]
-        //     }
-        // ]
-
-        res.status(201).json(returnDatas);
-    } catch (error) {
-        await client.query('ROLLBACK');
-        res.status(500).json({ error: error.message });
-    } finally {
-        client.release();
+    // check les entrées
+    if (!volunteerId) {
+      return res.status(400).json({ error: "Volunteer id is missing" });
     }
-})
+    if (!Array.isArray(quantitiesArray) || quantitiesArray.length === 0) {
+      return res.status(400).json({ error: "Quantities are missing" });
+    }
+    if (!cityId) {
+      return res.status(400).json({ error: "City id is missing" });
+    }
+
+    // Lance les query
+    await client.query("BEGIN");
+
+    // add collection
+    const insertCollectionQuery = `INSERT INTO collections (volunteer_id, city_id) VALUES ($1, $2) RETURNING id, created_at`;
+    // get city title
+    const selectCityTitleQuery = `SELECT title FROM cities WHERE id = $1`;
+    // add quantities
+    const insertQuantityQuery = `INSERT INTO quantities (quantity, waste_id, collection_id) VALUES ($1, $2, $3)`;
+    // get waste point value and title
+    const selectWasteQuery = `SELECT points_value, title, label FROM wastes WHERE id = $1`;
+    // get points volunteers already have
+    const selectVolunteerPointsQuery = `SELECT points FROM volunteers WHERE id = $1`;
+    // update volunteer / calculate total points
+    const updateVolunteerPointsQuery = `UPDATE volunteers SET points = $1 WHERE id = $2`;
+
+    const createCollecte = await client.query(insertCollectionQuery, [
+      volunteerId,
+      cityId,
+    ]);
+    const collectionId = createCollecte.rows[0].id;
+    const collectionDate = createCollecte.rows[0].created_at;
+
+    const getCity = await client.query(selectCityTitleQuery, [cityId]);
+    const cityName = getCity.rows[0].title;
+
+    const getSoldePoints = await client.query(selectVolunteerPointsQuery, [
+      volunteerId,
+    ]);
+    let totalPoints = getSoldePoints.rows[0].points;
+
+    const collectWastes = [];
+    let pointsEarned = 0;
+
+    for (const quantity of quantitiesArray) {
+      await client.query(insertQuantityQuery, [
+        quantity.quantity,
+        quantity.wasteId,
+        collectionId,
+      ]);
+
+      const wastePoints = await client.query(selectWasteQuery, [
+        quantity.wasteId,
+      ]);
+      pointsEarned += wastePoints.rows[0].points_value * quantity.quantity;
+
+      // faire le tableau : waste title, quantity
+      collectWastes.push({
+        title: wastePoints.rows[0].title,
+        label: wastePoints.rows[0].label,
+        quantity: quantity.quantity,
+      });
+    }
+
+    totalPoints += pointsEarned;
+
+    await client.query(updateVolunteerPointsQuery, [totalPoints, volunteerId]);
+    await client.query("COMMIT");
+
+    const returnDatas = [
+      {
+        date: collectionDate,
+        cityName,
+        collectWastes,
+      },
+    ];
+
+    // return [
+    //     {
+    //         date: "",
+    //         cityName: "",
+    //         [
+    //             {
+    //                 title: "",
+    //                 quantity: ""
+    //             },
+    //             ...
+    //         ]
+    //     }
+    // ]
+
+    res.status(201).json(returnDatas);
+  } catch (error) {
+    await client.query("ROLLBACK");
+    res.status(500).json({ error: error.message });
+  } finally {
+    client.release();
+  }
+});
 
 module.exports = router;
